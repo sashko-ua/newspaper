1a8c8189786b935f48a58b163a0db7c28a563126
Sashko Shablenskyi <56129170+sashko-ua-94@users.noreply.github.com>
2020-05-16 21:39:30 +0300

    add js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,21 @@
+// І з нову привіт мій таємний читачу. 
+// В завданні написано зробити невеличку галерею не використовуючи сторонні плагіни,
+// але я, на жаль, завжди брав готові рішення. 
+// Я лише нещодавно сів за вивчкння JS. 
+// Тому погугливши трішки (10-12 годин) зміг видавити зі своєї голови лише ось ЦЕ...
+// Мені здається, що я не розумію лише якоїсь маленької деталі, яка не дає можливості користуватися данними з масиву,
+// та підставляти їх в SRC.
+// Якщо є можливість дати хоча б якийсь фідбек, то ось номер телефону +3806835518885.
+// Дякую за увангу=)
+
 "use strict";
 $(document).ready(function(){
 
+    const images = ['../img/gif/car.gif', '../img/gif/horrore.gif', '../img/gif/phone.gif', '../img/gif/tenor.gif'];
+
+    let active = images[0];
+
     $(".img").click(function() {
-        const images = ['../img/gif/car.gif', '../img/gif/horrore.gif', '../img/gif/phone.gif', '../img/gif/tenor.gif'];
-
-        let active = images[0];
-
         $("body").append("<div class='modal'>"+
         "<img id='mainSrc' src='"+active+"'class='modal__content'/>"+
         "<div class='modal__close'>"+"&times;"+"</div>"+
@@ -13,13 +23,13 @@ $(document).ready(function(){
         "<div id='left' class='modal__left'></div>");
 
         $('#right').click(function(){
-            $('#mainSrc').attr('src', 'active');
+            $('#mainSrc').attr('src', active = images[2]);
         });
 
         $('#left').click(function(){
-            $('#mainSrc').attr('src', );
+            $('#mainSrc').attr('src', active = images[0]);
         });
-        
+
         $(".modal").fadeIn(800);
 
         $(".modal__close").click(function(){
